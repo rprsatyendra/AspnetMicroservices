1eb5ebe13056298169c22c8c61b58c7ca9b51aaa
satyendra <ratnagupta120@outlook.com>
2021-09-29 11:53:22 +0530

    Added serilog to seed logs on Elastic Search and Kibana

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,7 +1,9 @@
+using Common.Logging;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,17 +22,18 @@ namespace OcelotApiGw
             Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
-                    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json",true,true);
+                    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 })
-                .ConfigureLogging((hostingContext, loggingBuilder) =>
-                {
-                    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                    loggingBuilder.AddConsole();
-                    loggingBuilder.AddDebug();
-                });
+                .UseSerilog(SeriLogger.Configure);
+                //.ConfigureLogging((hostingContext, loggingBuilder) =>
+                //{
+                //    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                //    loggingBuilder.AddConsole();
+                //    loggingBuilder.AddDebug();
+                //});
     }
 }
